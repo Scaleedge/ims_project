6e2e1fb837910643fbc39db17ee9699c4e305c3e
sumitsahu26 <sumit.sohan@aietm.com.com>
2023-07-19 16:36:53 +0530

    add stock In/out module

---
+++ controllers/stockInOutController.js
@@ -0,0 +1,58 @@
+const db = require("../models")
+const StockInOut = db.stockInOut
+const ProductStock = db.productStock
+
+
+const stockInOut = async (req, res) => {
+
+    try {
+
+        const info = {
+            itemId: req.body.itemId,
+            outletId: req.body.outletId,
+            type: req.body.type,
+            qty: req.body.qty,
+            remarks: req.body.remarks
+        }
+
+
+        const productStock = await ProductStock.findOne({ where: { itemId: req.body.itemId  }})
+       
+
+        if(!productStock) {
+            res.status(500).send({
+                success: false,
+                message: "ProductStock is not found"
+            })
+        }
+
+        if (req.body.inOutFlag == "in") {
+
+            const addproductStock = await ProductStock.update( { freeQty : productStock.freeQty + 1 }, {where : {itemId : req.body.itemId}} )
+            
+
+        }
+
+        const removeProductStock = await ProductStock.update({ freeQty : productStock.freeQty - 1 }, {where : {itemId : req.body.itemId}})
+        
+
+        const stockInOut = await StockInOut.create(info)
+        req.flash('message', 'Stock added sucessfully');
+        return res.redirect('/stockInOut')
+
+    }
+
+    catch (err) {
+        res.status(500).send({
+            success: false,
+            // message: "something went wrong",
+            message: err.message
+
+        })
+        console.log(err)
+    }
+
+}
+module.exports = {
+    stockInOut
+}
---
+++ models/stockInOutModel.js
@@ -0,0 +1,33 @@
+const { DataTypes } = require("sequelize");
+const { sequelize, user } = require(".");
+
+module.exports = (sequelize, DataTypes) => {
+    
+    const StockInOut = sequelize.define('stock_in_out', {
+        id: {
+            type: DataTypes.BIGINT(20),
+            allowNull: false,
+            primaryKey: true,
+            autoIncrement : true
+        },
+        itemId : {
+            type : DataTypes.STRING(255),
+        },
+        outletId : {
+            type : DataTypes.STRING(255),
+        },
+        type : {
+            type : DataTypes.STRING,
+            defaultValue : "in"
+        },
+        qty : {
+            type : DataTypes.STRING,
+        },
+        remarks : {
+            type : DataTypes.STRING,
+        },
+    
+    })
+
+    return StockInOut
+}
